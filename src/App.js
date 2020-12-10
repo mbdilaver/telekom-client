b1642ea67d3b29e9f9a5d634147f1f76c76d91ec
MBD <mbdilaver@gmail.com>
2020-12-10 20:19:53 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
 
-function App() {
+const Board = () => {
+  const [client, setClient] = useState();
+  const [state, setState] = useState({ topic: '', messages: [] });
+
+  useEffect(() => {
+    var socket = new SockJS('http://localhost:8080/chat');
+    const stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, (frame) => {
+      console.log('Connected: ' + frame);
+    });
+
+    setClient(stompClient);
+  }, []);
+
+  const onConnect = () => {
+    client.subscribe('/notifications/' + state.topic, (msg) => {
+      setState({
+        ...state,
+        messages: [...state.messages, msg.body],
+      });
+    });
+  };
+
+  const onTopicChange = (e) => {
+    const topic = e.target.value;
+    setState({
+      ...state,
+      topic: topic,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type='text' onChange={onTopicChange} value={state.topic} />
+      <button onClick={onConnect}>Connect</button>
+      <p style={{ whiteSpace: 'pre-line' }}>{state.messages}</p>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr' }}>
+      <Board />
+      <Board />
+      <Board />
+    </div>
+  );
+};
 
 export default App;
